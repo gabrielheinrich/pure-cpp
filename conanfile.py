28d7b4cf6061ba6c8cf33dfc4c1ffab6dc9338ab
Gabriel Heinrich <info@eden-elements.com>
2019-03-26 09:59:55 +0100

    Updated Conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,12 @@ from conans import ConanFile, CMake, tools
 class Pure_Cpp_Conan (ConanFile) :
     name = "pure-cpp"
     version = "0.0.1"
-    generators = "cmake"
     exports_sources = "*"
+    url = "https://github.com/gabrielheinrich/pure-cpp"
+    description = "C++ Library for gradually-typed and data-oriented functional programming"
+    license = "https://github.com/gabrielheinrich/pure-cpp/blob/master/LICENSE"
+    author = "Gabriel Heinrich <gabriel@pure-cpp.org"
+    generators = "cmake"
     
     def build(self):
         cmake = CMake(self)
@@ -12,6 +16,7 @@ class Pure_Cpp_Conan (ConanFile) :
         cmake.build(target="check")
 
     def package (self):
+        self.copy ("LICENSE")
         self.copy ("*", src="include/pure", dst="include/pure")
         self.copy ("*", src="include/immer/immer", dst="include/immer")
 
